e4a1774c0e3d01b6e010135c88c07809addc2a82
Erwan <erwan.launay77@gmail.com>
2022-03-02 10:04:59 +0100

    Test API Axios front to back

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Notes from "../Notes";
 import scoot from "../../images/scooter.jpg";
 import images from "../../images/Gants-1.png";
 import images1 from "../../images/Blouson-1.jpg";
 import images2 from "../../images/Pantalon.jpg";
-import Navbar from '../Navbar';
 import "./styles.css";
+import axios from "axios";
 
 const Home = () => {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const fetchData = () => {
+    axios
+      .get('localhost:8000/api/listeProduit/')
+      .then((response) => {
+        console.log(response.data);
+        setIsLoading(false);
+        setNotes(response.data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
     console.log(images);
-  
     return (
       <div>
         <Container id="home">
-        <img className="middle" src={scoot} />
+        <h1>Using Axios</h1>
+      {notes && <Notes data={notes} />}
+      {isError && <div>Error fetching data.</div>}
+        <img className="middle" src={scoot} alt=""/>
           <Row className="mt-4">
             <Col md={6} className="center">
               <a href='/liste-des-produits'>
                 <h2>Gants</h2>
-                <img src={images} />
+                <img src={images} alt="" />
               </a>
             </Col>
             <Col md={6} className="center">
             <a href='/liste-des-produits'>
                 <h2>Blouson</h2>
-                <img src={images1} />
+                <img src={images1} alt="" />
               </a>
             </Col>
           </Row>
@@ -32,7 +58,7 @@ const Home = () => {
             <Col md={6} className="center">
             <a href='/liste-des-produits'>
                 <h2>Pantalon</h2>
-                <img src={images2} />
+                <img src={images2} alt="" />
               </a>
             </Col>
           </Row>
