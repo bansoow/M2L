b7acd440ef40949f0d0f99e2cbdc002bb46fd389
Erwan <erwan.launay77@gmail.com>
2022-03-02 16:02:22 +0100

    Ajout controllers

--- backend/controllers/listeProduit.js
+++ backend/controllers/listeProduit.js
@@ -5,7 +5,7 @@ module.exports = {
         let connexion;
         try {
             connexion = await pool.getConnection();
-            const result = await connexion.query('SELECT * FROM t_client;');
+            const result = await connexion.query('SELECT * FROM t_produit;');
             return res.status(200).json( result )
         } catch (error) {
             return res.status(400).json({ error: error.message });
