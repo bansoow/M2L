685f8b91eb37a44841f13a6dcc03eb996ddb6c1b
Matteo Beaulieu <m.beaulieu@ecole-ipssi.net>
2022-03-02 17:22:02 +0100

    BackEnd ListeProduit

--- src/pages/ListeProduit/index.js
+++ src/pages/ListeProduit/index.js
@@ -12,19 +12,21 @@ const ListeProduit = () => {
 
   let nomProduit = (
     produit.map(element => {
-    return <h4>{element.nom_produit}</h4>
-    return <h6>{element.marque_produit}</h6>
+    return <div className="vignette">
+            <a className="redirect" href="/produit">
+            <img src={images} />
+            </a>
+            <div className="subTitle">
+            <div>
+            <h4>{element.nom_produit}</h4>
+            <h6>{element.marque_produit}</h6>
+            </div>
+            <div className='price'>
+            <h3>{element.prix_produit}</h3>
+            </div>
+          </div>
+          </div>
   })
-  )
-  let marqueProduit = (
-    produit.map(element => {
-      return <h6>{element.marque_produit}</h6>
-    })
-  )
-  let prixProduit = (
-    produit.map(element => {
-      return <h3>{element.prix_produit}</h3>
-    })
   )
   return (
       <div id="listeProduit">
@@ -34,88 +36,7 @@ const ListeProduit = () => {
             <h2>Voir plus</h2>
           </div>
           <div className='flex-vignette'>
-            <div className="vignette">
-              <a className="redirect" href="/produit">
-              <img src={images} />
-              </a>
-              <div className="subTitle">
-                <div>
-                  {nomProduit}
-                  {marqueProduit}
-                </div>
-                <div className='price'>
-                  {prixProduit}
-                </div>
-              </div>
-            </div>
-            <div className="vignette">
-              <a className="redirect" href="/produit">
-              <img src={images} />
-              </a>
-              <div className="subTitle">
-                <div>
-                  <h4>Gants jet D30 noir</h4>
-                  <h6>PARIS 17 - il y a deux jours</h6>
-                </div>
-                <div className='price'>
-                  <h3>12€</h3>
-                </div>
-              </div>
-            </div>
-            <div className="vignette">
-              <a className="redirect" href="/produit">
-              <img src={images} />
-              </a>
-              <div className="subTitle">
-                <div>
-                  <h4>Gants jet D30 noir</h4>
-                  <h6>PARIS 17 - il y a deux jours</h6>
-                </div>
-                <div className='price'>
-                  <h3>12€</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="liste">
-          <div className="title">
-            <h2>Gants été</h2>
-            <h4>Voir plus</h4>
-          </div>
-          <div className="vignette">
-            <a className="redirect" href="/produit">
-            <img src={images} />
-            </a>
-            <div className="subtTitle">
-              <div>
-                <h4>Gants jet D30 noir</h4>
-                <h6>PARIS 17 - il y a deux jours</h6>
-              </div>
-              <div>
-                <h3>12€</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="liste">
-          <div className="title">
-            <h2>Gants hiver</h2>
-            <h4>Voir plus</h4>
-          </div>
-          <div className="vignette">
-            <a className="redirect" href="/produit">
-            <img src={images} />
-            </a>
-            <div className="subtTitle">
-              <div>
-                <h4>Gants jet D30 noir</h4>
-                <h6>PARIS 17 - il y a deux jours</h6>
-              </div>
-              <div>
-                <h3>12€</h3>
-              </div>
-            </div>
+              {nomProduit}
           </div>
         </div>
       </div>
